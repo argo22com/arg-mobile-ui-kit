1668c075602400a6fd8dab5aeeb7c2b9c0de4923
Kamony <jirihauser94@gmail.com>
2024-08-02 15:18:01 +0200

    fix: typescript const theme type

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -52,7 +52,7 @@ export const colors = {
         800: "#04443b",
     },
     overlay: "rgba(0, 0, 0, 0.3)"
-} as const;
+}
 
 export const borderRadiuses = {
     none: 0,
@@ -73,7 +73,7 @@ export const spacings = {
     lg: 24,
     xl: 32,
     "2xl": 48,
-} as const;
+}
 
 export const fonts = {
     paragraph: {
@@ -84,7 +84,7 @@ export const fonts = {
         light: "FrauncesRegular",
         strong: "FrauncesBold",
     },
-} as const;
+}
 
 export const fontSizes = {
     xs: 10,
@@ -93,12 +93,13 @@ export const fontSizes = {
     lg: 18,
     xl: 24,
     "2xl": 24,
-} as const;
+}
 
 export const borderWidth = {
+    none: 0,
     sm: 1,
     md: 2,
-} as const;
+}
 
 export const defaultTheme = {
     colors,
@@ -363,4 +364,4 @@ export const defaultTheme = {
             },
         },
     },
-} as const;
+}
