a986797be0cee020804782216aee48dfa969c9d7
Martin Novák <martin.novak@argo22.com>
2024-10-18 11:59:58 +0200

    feat: Icon button example
    - created icon button example and added it to root

--- example/src/Root.tsx
+++ example/src/Root.tsx
@@ -1,18 +1,19 @@
-import { ScrollView, View } from "react-native";
 import { Typography } from "@argo22/mobile-ui-kit";
+import { ScrollView, View } from "react-native";
 import { createStyleSheet, useStyles } from "react-native-unistyles";
 import { ButtonExample } from "./examples/Button.example";
-import { SwitchExample } from "./examples/Switch.example";
+import { CardExample } from "./examples/Card.example";
 import { CheckboxExample } from "./examples/Checkbox.example";
 import { IconExample } from "./examples/Icon.example";
 import { InputExample } from "./examples/Input.example";
+import { LoaderExample } from "./examples/Loader.example";
 import { RadioGroupExample } from "./examples/RadioGroup.example";
 import { SelectExample } from "./examples/Select.example";
+import { SwitchExample } from "./examples/Switch.example";
 import { ToastExample } from "./examples/Toast.example";
 import { ToastManagerExample } from "./examples/ToastManager.example";
 import { TypographyExample } from "./examples/Typography.example";
-import {LoaderExample} from "./examples/Loader.example";
-import {CardExample} from "./examples/Card.example";
+import { IconButtonExample } from "./examples/IconButton.example";
 
 export const Root = () => {
   const { styles } = useStyles(stylesheet);
@@ -21,6 +22,7 @@ export const Root = () => {
     <Typography size={"xl"}>@Argo22/mobile-ui-kit Example</Typography>
     <View style={{gap: 24, marginTop: 24}}>
       <CardExample/>
+      <IconButtonExample/>
       <ButtonExample />
       <CheckboxExample />
       <ToastExample />
